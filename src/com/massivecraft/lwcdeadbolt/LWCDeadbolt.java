5b4798c99c1ec0d6c23aac0511c0ba0ca4141c10
yendor46 <porter.rodney@sbcglobal.net>
2014-01-11 23:47:15 -0800

    Initial working release.

---
+++ src/com/massivecraft/lwcdeadbolt/LWCDeadbolt.java
@@ -0,0 +1,48 @@
+package com.massivecraft.lwcdeadbolt;
+
+import java.util.logging.Logger;
+
+import org.bukkit.plugin.Plugin;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import com.griefcraft.lwc.LWC;
+
+public class LWCDeadbolt extends JavaPlugin
+{
+	private Logger logger = Logger.getLogger("LWC-Deadbolt");
+
+	/**
+	 * Initialize LWCDeadbolt
+	 */
+	public void init()
+	{
+		LWC.getInstance().getModuleLoader()
+				.registerModule(this, new LWCDeadboltModule(this));
+		log("Hooked into LWC!");
+	}
+
+	public void onEnable()
+	{
+		Plugin lwc = getServer().getPluginManager().getPlugin("LWC");
+
+		if (lwc != null)
+		{
+			init();
+		}
+		else
+		{
+
+			log("Waiting for LWC to be enabled...");
+		}
+	}
+
+	public void onDisable()
+	{
+
+	}
+
+	private void log(String message)
+	{
+		logger.info("LWC-Deadbolt: " + message);
+	}
+}
---
+++ src/com/massivecraft/lwcdeadbolt/LWCDeadboltModule.java
@@ -0,0 +1,38 @@
+package com.massivecraft.lwcdeadbolt;
+
+import com.griefcraft.lwc.LWC;
+import com.griefcraft.scripting.JavaModule;
+import com.griefcraft.scripting.event.LWCProtectionRegisterEvent;
+import com.daemitus.deadbolt.Deadbolt;
+
+public class LWCDeadboltModule extends JavaModule
+{
+
+	/**
+	 * The bukkit plugin
+	 */
+	@SuppressWarnings("unused")
+	private LWCDeadbolt plugin;
+	/**
+	 * The LWC object, set by load ()
+	 */
+	@SuppressWarnings("unused")
+	private LWC lwc;
+
+	public LWCDeadboltModule(LWCDeadbolt plugin)
+	{
+		this.plugin = plugin;
+	}
+
+	@Override
+	public void onRegisterProtection(LWCProtectionRegisterEvent event)
+	{
+
+		String owner = Deadbolt.getOwnerName(event.getBlock());
+		String player = event.getPlayer().getName();
+		player = player.substring(0, Math.min(player.length(), 15));
+		if (owner.equalsIgnoreCase(player)) return;
+		event.setCancelled(true);
+
+	}
+}
